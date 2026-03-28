e0bfd893a6e462a8a2559d8e7321189d109c6113
sourav <souravmahanty12@gmail.com>
2026-03-28 18:54:57 +0530

    bug fix

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,10 @@
+
 import admin from "firebase-admin";
-import serviceAccount from "../config/serviceAccountKey.json";
+
+// Load service account from environment variable (stringified JSON)
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : undefined;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
